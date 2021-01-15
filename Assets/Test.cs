b410a7354623014171339c3db1c137bfc813b014
HolyKritGrenade <kythomas35@gmail.com>
2021-01-15 20:51:19 +0800

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        go = new GameObject();
+        go1 = new GameObject();
     }
 
     // Update is called once per frame
