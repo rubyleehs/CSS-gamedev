6583eb2a00a03890ffb2ffad730ce8bc5e7c12ac
EsCharl <63916811+EsCharl@users.noreply.github.com>
2021-01-20 15:10:48 +0800

    Character Flip (unfinished)
    rotate left and right cause the character to disapear

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,10 @@ public class Player : Agent
     public int hp = 10, ammo = 5;
 
     private Animator animator;
+    private bool m_FacingRight = true;
+    private bool m_FacingUp = false;
+    private bool m_FacingLeft = false;
+    private bool m_FacingDown = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +31,32 @@ public class Player : Agent
             GameManager.instance.TogglePause();
 
         // shooting projectile
-        if (Input.GetKeyDown("k") && ammo != 0)
+        if (Input.GetButtonDown("Fire1") && ammo != 0)
         {
             animator = GetComponent<Animator>();
 
             ammo--;
 
-            ammoText.text = "Ammo: " + ammo + " - 1";
+            ammoText.text = "Ammo: " + ammo;
         }
 
-        int horizontal = (int) (Input.GetAxisRaw("Horizontal"));
-        int vertical = (int) (Input.GetAxisRaw("Vertical"));
+        int horizontal = 0;
+        int vertical = 0;
+
+        vertical = (int) (Input.GetAxisRaw("Vertical"));
+        horizontal = (int) (Input.GetAxisRaw("Horizontal"));
 
         if (vertical != 0)
             horizontal = 0;
+
+        if ((m_FacingRight == true && horizontal == -1) || (m_FacingLeft == true && horizontal == 1))
+            HFlip();
+        else if ((m_FacingDown == true && vertical == 1) || m_FacingUp == true && vertical == -1)
+            VFlip();
+        else if ((m_FacingRight == true && vertical == 1) || (m_FacingUp == true && horizontal == -1) || (m_FacingLeft == true && vertical == -1) || (m_FacingDown == true && horizontal == 1))
+            rotateLeft();
+        else if ((m_FacingUp == true && horizontal == 1) || (m_FacingRight == true && vertical == -1) || (m_FacingDown == true && horizontal == -1) || (m_FacingLeft == true && vertical == 1))
+            rotateRight();
 
         Vector2Int direction = new Vector2Int(horizontal, vertical);
 
@@ -54,7 +70,7 @@ public class Player : Agent
         {
             ammo += pointsPerAmmo;
 
-            ammoText.text = "Ammo: " + ammo + "+ " + pointsPerAmmo;
+            ammoText.text = "Ammo: " + ammo;
 
             collision.gameObject.SetActive(false);
         }
@@ -63,7 +79,7 @@ public class Player : Agent
         {
             hp += pointsPerHealthKit;
 
-            healthText.text = "Health: " + hp + " + " + pointsPerHealthKit;
+            healthText.text = "Health: " + hp;
 
             collision.gameObject.SetActive(false);
         }
@@ -84,5 +100,95 @@ public class Player : Agent
         {
 
         }
+    }
+
+    private void HFlip()
+    {
+        if (m_FacingRight)
+        {
+            m_FacingRight = !m_FacingRight;
+            m_FacingLeft = !m_FacingLeft;
+        }
+        else if (m_FacingLeft)
+        {
+            m_FacingLeft = !m_FacingLeft;
+            m_FacingRight = !m_FacingRight;
+        }
+
+        m_FacingUp = false;
+        m_FacingDown = false;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void VFlip()
+    {
+        if (m_FacingUp)
+        {
+            m_FacingUp = !m_FacingUp;
+            m_FacingDown = !m_FacingDown;
+        }
+        else if (m_FacingDown)
+        {
+            m_FacingDown = !m_FacingDown;
+            m_FacingUp = !m_FacingUp;
+        }
+
+        m_FacingRight = false;
+        m_FacingLeft = false;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void rotateLeft()
+    {
+        if (m_FacingRight == true)
+        {
+            m_FacingRight = !m_FacingRight;
+            m_FacingUp = true;
+        }
+        else if (m_FacingUp == true)
+        {
+            m_FacingUp = !m_FacingUp;
+            m_FacingLeft = true;
+        }
+        else if (m_FacingLeft == true)
+        {
+            m_FacingLeft = !m_FacingLeft;
+            m_FacingDown = true;
+        }
+        else if (m_FacingDown == true)
+        {
+            m_FacingDown = !m_FacingDown;
+            m_FacingRight = true;
+        }
+
+        transform.Rotate(0f, -90f, 0f);
+    }
+
+    private void rotateRight()
+    {
+        if (m_FacingRight == true)
+        {
+            m_FacingRight = !m_FacingRight;
+            m_FacingDown = true;
+        }
+        else if (m_FacingUp == true)
+        {
+            m_FacingUp = !m_FacingUp;
+            m_FacingRight = true;
+        }
+        else if (m_FacingLeft == true)
+        {
+            m_FacingLeft = !m_FacingLeft;
+            m_FacingUp = true;
+        }
+        else if (m_FacingDown == true)
+        {
+            m_FacingDown = !m_FacingDown;
+            m_FacingLeft = true;
+        }
+
+        transform.Rotate(0f, 90f, 0f);
     }
 }
