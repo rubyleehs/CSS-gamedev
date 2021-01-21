5d2292db77cee9bc0bbe547780f884ef44021775
SOOPG <77486317+SOOPG@users.noreply.github.com>
2021-01-21 20:30:20 +0800

    Included Ammo Pickup Frame (Incompleted)
    What are the lines that should be changed?

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour, IPlayerInteractable
+{
+    bool isStillDangerous = true;
+
+    public bool CanInteract(Agent agent)
+    {
+        return isStillDangerous;
+    }
+
+    public void Interact(Agent agent)
+    {
+        if (!CanInteract(agent))
+            return; 
+
+        if (agent is Player)
+        {
+            Player player = (Player)agent;
+            player.ChangeAmmoAmount(10);
+            //change player hp
+        }
+    }
+}
+
+
+public class HealthPickup : MonoBehaviour, IPlayerInteractable
+{
+    bool isFromPlayer = true; //check if is picked up by player
+
+    public bool CanInteract(Player player) => isFromPlayer;
+
+    public void Interact(Player player)
+    {
+        if (!CanInteract(player))
+            return;
+
+        if (player is Player)
+        {
+            Player player = (Player)player;
+            player.Changehp(10);
+            //change player hp
+        }
+    }
+}
+
+
+
+public class AmmoPickup : MonoBehaviour, IPlayerInteractable
+{
+    bool isFromPlayer = true; //check if is picked up by player
+
+    public bool CanInteract(Player player) => isFromPlayer;
+
+    public void Interact(Player player)
+    {
+        if (!CanInteract(player))
+            return;
+
+        if (player is Player)
+        {
+            Player player = (Player)player;
+            player.Changeammo(5);
+            //change player ammo
+        }
+    }
+}
