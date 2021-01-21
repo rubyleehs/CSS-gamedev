5720c8b6088f9f7e9a246b14d6a6b94ee9b28b9b
SOOPG <77486317+SOOPG@users.noreply.github.com>
2021-01-21 17:38:33 +0800

    Revert "Additions in Health Pickup"
    This reverts commit 5a2fc25a29b5cd6455864727931092e0fb6be9fe.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,9 @@ public class Player : Agent
     public int pointsPerHealthKit = 1;
     public Text healthText;
     public Text ammoText;
-    public Text addingAmmo;
-    public Text addingHealth;
-    public LineRenderer lineRenderer;
-    public Transform firePoint;
     public int hp = 10, ammo = 5;
 
     private Animator animator;
-    private bool m_FacingRight = true;
-    private bool m_FacingUp = false;
-    private bool m_FacingLeft = false;
-    private bool m_FacingDown = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,199 +27,62 @@ public class Player : Agent
             GameManager.instance.TogglePause();
 
         // shooting projectile
-        if (Input.GetButtonDown("Fire1") && ammo != 0)
+        if (Input.GetKeyDown("k") && ammo != 0)
         {
-            StartCoroutine(Shoot());
+            animator = GetComponent<Animator>();
+
+            ammo--;
+
+            ammoText.text = "Ammo: " + ammo + " - 1";
         }
 
-        int horizontal = 0;
-        int vertical = 0;
-
-        vertical = (int) (Input.GetAxisRaw("Vertical"));
-        horizontal = (int) (Input.GetAxisRaw("Horizontal"));
+        int horizontal = (int) (Input.GetAxisRaw("Horizontal"));
+        int vertical = (int) (Input.GetAxisRaw("Vertical"));
 
         if (vertical != 0)
             horizontal = 0;
-
-        if ((m_FacingRight == true && horizontal == -1) || (m_FacingLeft == true && horizontal == 1))
-            HFlip();
-        else if ((m_FacingDown == true && vertical == 1) || m_FacingUp == true && vertical == -1)
-            VFlip();
-        else if ((m_FacingRight == true && vertical == 1) || (m_FacingUp == true && horizontal == -1) || (m_FacingLeft == true && vertical == -1) || (m_FacingDown == true && horizontal == 1))
-            rotateLeft();
-        else if ((m_FacingUp == true && horizontal == 1) || (m_FacingRight == true && vertical == -1) || (m_FacingDown == true && horizontal == -1) || (m_FacingLeft == true && vertical == 1))
-            rotateRight();
 
         Vector2Int direction = new Vector2Int(horizontal, vertical);
 
         if(vertical != 0 || horizontal != 0) {
             base.Move(direction);
         }
-
-        healthText.text = "Health: " + hp;
-        ammoText.text = "Ammo: " + ammo;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Interactable")
+        if (collision.tag == "Ammo")
         {
-            IPlayerInteractable playerInteractable = collision.gameObject.GetComponent<IPlayerInteractable>();
-            if (playerInteractable != null)
-            {
-                playerInteractable.Interact(this);
-            }
+            ammo += pointsPerAmmo;
+
+            ammoText.text = "Ammo: " + ammo + "+ " + pointsPerAmmo;
+
+            collision.gameObject.SetActive(false);
         }
+
+        if (collision.tag == "HealthKit")
+        {
+            hp += pointsPerHealthKit;
+
+            healthText.text = "Health: " + hp + " + " + pointsPerHealthKit;
+
+            collision.gameObject.SetActive(false);
+        }
+
+        if (collision.tag == "Hole")
+            hp = 0;
     }
 
-    public void ChangeAmmoAmount(int delta)
+    private void ChangeAmmoAmount(int delta)
     {
         ammo += delta;
         ammoText.text = "Ammo: " + ammo;
-
-        addingAmmo.text = "+ " + delta;
     }
 
-    public void ChangeHealthAmount(int delta)
-    {
-        hp += delta;
-        healthText.text = "Health: " + hp;
-
-        addingHealth.text = "+ " + delta;
-    }
-
-    public void Damaged (int delta)
-    {
-        hp -= delta;
-
-        healthText.text = "Health: " + hp;
-        addingHealth.text = "- " + delta;
-    }
-
-    IEnumerator Shoot()
-    {
-
-        ammo--;
-
-        ammoText.text = "Ammo: " + ammo;
-
-        addingAmmo.text = "- 1";
-
-        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
-
-        if (hitInfo)
-        {
-            lineRenderer.SetPosition(0, firePoint.position);
-            lineRenderer.SetPosition(1, hitInfo.point);
-        }
-        else
-        {
-            lineRenderer.SetPosition(0, firePoint.position);
-            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
-        }
-
-        lineRenderer.enabled = true;
-
-        yield return new WaitForSeconds(.02f);
-
-        lineRenderer.SetVertexCount(0);
-    }
-
-    private void GameOver()
+    private void Lost()
     {
         if (hp <= 0)
         {
 
         }
-    }
-
-    private void HFlip()
-    {
-        if (m_FacingRight)
-        {
-            m_FacingRight = !m_FacingRight;
-            m_FacingLeft = !m_FacingLeft;
-        }
-        else if (m_FacingLeft)
-        {
-            m_FacingLeft = !m_FacingLeft;
-            m_FacingRight = !m_FacingRight;
-        }
-
-        m_FacingUp = false;
-        m_FacingDown = false;
-
-        transform.Rotate(0f, 0f, 180f);
-    }
-
-    private void VFlip()
-    {
-        if (m_FacingUp)
-        {
-            m_FacingUp = !m_FacingUp;
-            m_FacingDown = !m_FacingDown;
-        }
-        else if (m_FacingDown)
-        {
-            m_FacingDown = !m_FacingDown;
-            m_FacingUp = !m_FacingUp;
-        }
-
-        m_FacingRight = false;
-        m_FacingLeft = false;
-
-        transform.Rotate(0f, 0f, 180f);
-    }
-
-    private void rotateLeft()
-    {
-        if (m_FacingRight == true)
-        {
-            m_FacingRight = !m_FacingRight;
-            m_FacingUp = true;
-        }
-        else if (m_FacingUp == true)
-        {
-            m_FacingUp = !m_FacingUp;
-            m_FacingLeft = true;
-        }
-        else if (m_FacingLeft == true)
-        {
-            m_FacingLeft = !m_FacingLeft;
-            m_FacingDown = true;
-        }
-        else if (m_FacingDown == true)
-        {
-            m_FacingDown = !m_FacingDown;
-            m_FacingRight = true;
-        }
-
-        transform.Rotate(0f, 0f, 90f);
-    }
-
-    private void rotateRight()
-    {
-        if (m_FacingRight == true)
-        {
-            m_FacingRight = !m_FacingRight;
-            m_FacingDown = true;
-        }
-        else if (m_FacingUp == true)
-        {
-            m_FacingUp = !m_FacingUp;
-            m_FacingRight = true;
-        }
-        else if (m_FacingLeft == true)
-        {
-            m_FacingLeft = !m_FacingLeft;
-            m_FacingUp = true;
-        }
-        else if (m_FacingDown == true)
-        {
-            m_FacingDown = !m_FacingDown;
-            m_FacingLeft = true;
-        }
-
-        transform.Rotate(0f, 0f, -90f);
     }
 }
