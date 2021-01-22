365f9e2504ba44872cd4019be690b379c65c2580
SOOPG <77486317+SOOPG@users.noreply.github.com>
2021-01-22 20:54:29 +0800

    Addition of AmmoPickup
    Added ammo pickups

--- Assets/Scripts/Pickups.cs
+++ Assets/Scripts/Pickups.cs
@@ -2,30 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HealthPickup : MonoBehaviour, IAgentInteractable
-{
-    bool isStillDangerous = true;
-
-    public bool CanInteract (Agent agent)
-    {
-        return isStillDangerous;
-    }
-
-    public void Interact (Agent agent)
-    {
-        if (!CanInteract (agent))
-            return;
-
-        if (agent is Player)
-        {
-            Player player = (Player) agent;
-            player.ChangeAmmoAmount (10);
-            //change player hp
-        }
-    }
-}
-
-public class HealthPickup2 : MonoBehaviour, IAgentInteractable
 {
     public bool CanInteract (Agent agent)
     {
