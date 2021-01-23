ad4cfdd82affca957beb81a9b0bba7ee1a3a8c9d
EsCharl <63916811+EsCharl@users.noreply.github.com>
2021-01-23 14:16:26 +0800

    Enemy (incomplete)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,13 +8,13 @@ public class Enemy : Agent
 
     private Transform target;
     private Animator animator;
+    
+    int xDir;
+    int yDir;
 
     // Start is called before the first frame update
     void Start()
     {
-        int xDir = 0;
-        int yDir = 0;
-
         animator = GetComponent<Animator>();
 
         target = GameObject.FindGameObjectWithTag("Player").transform;
@@ -22,16 +22,19 @@ public class Enemy : Agent
         if ((int)target.position.x - (int) transform.position.x != 0)
         {
             xDir = (int)target.position.x > (int)transform.position.x ? 1 : -1;
+            yDir = 0;
         }
         else
         {
             yDir = (int)target.position.y > (int)transform.position.y ? 1 : -1;
+            xDir = 0;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2Int Moving = new Vector2Int(xDir, yDir);
+        base.Move(Moving);
     }
 }
