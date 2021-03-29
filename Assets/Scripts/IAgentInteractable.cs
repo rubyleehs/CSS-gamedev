af5e6712ba022486549c60b79a353b7983d8e1b1
SOOPG <77486317+SOOPG@users.noreply.github.com>
2021-03-30 00:54:50 +0800

    Fixed Error in Pickups of Workshop Script

---
+++ Assets/Scripts/IAgentInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAgentInteractable
+{  /// <summary>
+   /// Interface for any interactable an Agent can interact with
+   /// </summary>
+    bool CanInteract(Agent agent);
+    void Interact(Agent agent);
+}
