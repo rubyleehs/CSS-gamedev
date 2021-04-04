7b48793a2e10d914cd617676899416de199da752
HolyKritGrenade <kythomas35@gmail.com>
2021-04-04 20:37:37 +0800

    GameManager, Level Generator and all relevant prefabs created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    // Enum of game states
+    public enum GameState {
+        Play,
+        GameOver
+    }
+
+    // Public variables
+    public GameState currentState;
+    public LevelGenerator levelGen;
+    public float startingDifficultyLevel = 2;
+    public float difficultyScaling = 0.2f;
+
+    [HideInInspector]
+    public float difficultyLevel = 2;
+
+    // Private variables
+    // private List<Enemy> enemyList;
+    private bool paused = false;
+    private Text infoText;
+    private GameObject menuBackdrop;
+
+    public static GameManager instance;
+
+    /// <summary>
+    /// Sets up object references and the GameManager.
+    /// </summary>
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        levelGen = GetComponent<LevelGenerator>();
+
+        // Inits references
+        infoText = GameObject.Find("InfoText").GetComponent<Text>();
+        menuBackdrop = GameObject.Find("MenuBackdrop");
+    }
+
+    private void Start() {
+        InitGame();
+    }
+
+    /// <summary>
+    /// Sets up the game.
+    /// </summary>
+    private void InitGame() {
+        paused = false;
+        Time.timeScale = 1;
+        infoText.gameObject.SetActive(paused); // paused is always false here
+        menuBackdrop.SetActive(paused); // Disables black screen
+
+        currentState = GameState.Play;
+
+        // Sets up the camera
+        GameCamera.instance.ResetStats(); //GameCamera.instance is null if you are doing it in the workshop scene coz this code is in the completed namespace
+
+        // Sets up the player
+        Player.instance.ResetStats();
+
+        // Resets difficulty
+        difficultyLevel = startingDifficultyLevel;
+
+        // Sets up the level
+        levelGen.InitLevel(0);
+    }
+
+    /// <summary>
+    /// Displays game over screen and deletes level.
+    /// </summary>
+    public void InitGameOver() {
+        // Destroys the level
+        levelGen.DestroyLevel();
+
+        currentState = GameState.GameOver;
+        menuBackdrop.SetActive(true);
+        infoText.text = "Game Over. Play again? [ENTER]";
+        infoText.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Pause and resume. 
+    /// </summary>
+    public void TogglePause() {
+        paused = !paused;
+        infoText.text = "PAUSED";
+        infoText.gameObject.SetActive(paused);
+        Time.timeScale = paused ? 0 : 1;
+    }
+
+    /// <summary>
+    /// Manages pausing and decides on which gamestate to run.
+    /// (Play, GameOver)
+    /// </summary>
+    void Update() {
+        // Pause Game
+        if (Input.GetKeyDown("p"))
+            TogglePause();
+
+        // Game is simply not updated when paused
+        if (paused)
+            return;
+        else {
+            // Updates current game state
+            switch (currentState) {
+                case GameState.Play:
+                    Play();
+                    break;
+                case GameState.GameOver:
+                    GameOver();
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates the Player, generates chunks, destroys old chunks and increases difficulty level.
+    /// Active while game is being played, obviously.
+    /// </summary>
+    private void Play() {
+
+        // Generates new chunks ahead of the camera
+        if (levelGen.chunkCount * levelGen.CHUNK_ROWS < GameCamera.instance.transform.position.y + levelGen.CHUNK_ROWS) {
+            levelGen.SpawnChunk(difficultyLevel);
+        }
+
+        // Destroys chunks that go behind the camera
+        // TODO: THIS IS AN MVP METHOD. NOT ELEGANT AT ALL.
+        if (GameCamera.instance.transform.position.y - (levelGen.CHUNK_ROWS * 2) > levelGen.chunksDestroyed * levelGen.CHUNK_ROWS) {
+            levelGen.DestroyEarliestChunk();
+        }
+
+        // Updates difficulty level
+        difficultyLevel = Mathf.Max(difficultyLevel, levelGen.chunkCount * difficultyScaling);
+
+    }
+
+    /// <summary>
+    /// Wait for the Enter key to be pressed, then restarts the game.
+    /// Active during the GameOver screen.
+    /// </summary>
+    private void GameOver() {
+        if (Input.GetKeyDown("return"))
+            InitGame();
+    }
+}
+
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.Tilemaps;
+
+
+    /// <summary>
+    /// Class for generating levels.
+    /// OOP Challenge:
+    /// GameManager is currently very tightly coupled with this.
+    /// Try resolving it.
+    /// </summary>
+    public class LevelGenerator : MonoBehaviour {
+        public GameObject[] battleChunks;
+        public GameObject[] obstacleChunks;
+        public GameObject[] enemies;
+        public GameObject[] pickups;
+
+        [HideInInspector] public int chunkCount = 0;
+        [HideInInspector] public int chunksDestroyed = 0;
+
+        private int chunksBeforeBattle = 3;
+        private List<Vector3> availableTiles = new List<Vector3>();
+        private List<Vector3> entityTiles = new List<Vector3>();
+        private List<GameObject> chunkList = new List<GameObject>();
+
+        public int CHUNK_ROWS = 15;
+        public int CHUNK_COLUMNS = 15;
+
+        // Stores the level
+        public Transform level;
+
+        /// <summary>
+        /// Initializes the level. Spawns an initial chunk. 
+        /// </summary>
+        /// <param name="difficultyLevel">The initial difficulty of the game.</param>
+        public void InitLevel(int difficultyLevel) {
+            chunkCount = 0;
+            chunksDestroyed = 0;
+            SpawnChunk(difficultyLevel);
+        }
+
+        /// <summary>
+        /// Spawns a new level chunk ahead of the player.
+        /// </summary>
+        /// <param name="difficultyLevel">Difficulty level of the chunk to be spawned. 
+        /// Enemies are more numerous as difficulty increases.</param>
+        public void SpawnChunk(float difficultyLevel) {
+
+            int enemiesToSpawn;
+            GameObject spawnChunk;
+
+            // Determines type of chunk to spawn and enemy count
+            if (chunkCount % chunksBeforeBattle == 0) {
+                spawnChunk = battleChunks[Random.Range(0, battleChunks.Length)];
+                enemiesToSpawn = (int)difficultyLevel;
+            }
+            else {
+                spawnChunk = obstacleChunks[Random.Range(0, obstacleChunks.Length)];
+                enemiesToSpawn = (int)Mathf.Ceil(Mathf.Log(difficultyLevel, 2));
+            }
+
+            // Generates a new grid of available tiles minus the wall tiles
+            GenerateTilePositions(spawnChunk);
+
+            // Instantiates the chunk itself
+            GameObject chunkInstance = Instantiate(spawnChunk, new Vector3(0, chunkCount * CHUNK_ROWS, 0f), Quaternion.identity) as GameObject;
+            chunkInstance.transform.SetParent(level);
+            chunkList.Add(chunkInstance);
+
+            // Gets the entity list of the spawned chunk
+            Transform entityList = chunkInstance.transform.Find("Entities");
+
+            // TODO: Decides how many pickups to spawn, this is a placeholder
+            int pickupsToSpawn = 1;
+
+            // Spawns in enemies
+            spawnEnemies(enemiesToSpawn, entityList);
+
+            // Spawns in pickups
+            spawnPickups(pickupsToSpawn, entityList);
+
+            chunkCount++;
+        }
+
+        /// <summary>
+        /// Destroys the rearmost chunk behind the player.
+        /// This is done to avoid clutter.
+        /// </summary>
+        public void DestroyEarliestChunk() {
+            Destroy(chunkList[0]);
+            chunkList.RemoveAt(0);
+            chunksDestroyed++;
+        }
+
+        /// <summary>
+        /// Destroys the entire level.
+        /// </summary>
+        public void DestroyLevel() {
+            foreach (Transform child in level) {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Determines where entities can spawn within a chunk by scanning for walls.
+        /// Open tile locations are temporarily stored in the availableTiles list.
+        /// </summary>
+        /// <param name="spawnChunk">The chunk to scan for spawnpoints.</param>
+        void GenerateTilePositions(GameObject spawnChunk) {
+            availableTiles.Clear();
+
+            // Gets the walls from the chunk to be spawned
+            GameObject walls = spawnChunk.transform.Find("Walls").gameObject;
+            Tilemap wallTilemap = walls.GetComponent<Tilemap>();
+
+            for (int x = 0; x < CHUNK_COLUMNS; x++) {
+                for (int y = 0; y < CHUNK_ROWS; y++) {
+                    // If the tile does not exist in the wall tilemap, add it into list of available tiles
+                    Vector3Int tileTBAdded = new Vector3Int(x, y, 0);
+                    if (!wallTilemap.HasTile(tileTBAdded))
+                        availableTiles.Add((Vector3)tileTBAdded);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Randomly generates entity spawnpoints from available tiles.
+        /// Tiles to be spawned are temporarily stored in the entityTiles list.
+        /// </summary>
+        /// <param name="entitiesToSpawn">The number of entities to spawn</param>
+        void GenerateEntityPositions(int entitiesToSpawn) {
+            entityTiles.Clear();
+
+            for (int i = 0; i < entitiesToSpawn; i++) {
+
+                // Gets a random tile 
+                Vector3 entitySpawnLocation = availableTiles[Random.Range(0, availableTiles.Count)];
+
+                entityTiles.Add(entitySpawnLocation);
+
+                // Removes tile from pool
+                availableTiles.Remove(entitySpawnLocation);
+            }
+        }
+
+        /// <summary>
+        /// Spawns enemies into the current chunk.
+        /// </summary>
+        /// <param name="enemiesToSpawn">Number of enemies to spawn.</param>
+        /// <param name="entityList">Transform which is to parent the spawned Enemies.</param>
+        void spawnEnemies(int enemiesToSpawn, Transform entityList) {
+
+            // Generates positions for spawning enemies.
+            GenerateEntityPositions(enemiesToSpawn);
+
+            for (int i = 0; i < enemiesToSpawn; i++) {
+
+                Vector3 enemyPos = entityTiles[i];
+                GameObject spawnedEnemy = enemies[Random.Range(0, enemies.Length)];
+
+                enemyPos.y += (chunkCount * CHUNK_ROWS);
+
+                GameObject enemyInstance = Instantiate(spawnedEnemy, enemyPos, Quaternion.identity) as GameObject;
+
+                enemyInstance.transform.SetParent(entityList);
+
+            }
+        }
+
+        /// <summary>
+        /// Spawns pickups into a given chunk.
+        /// </summary>
+        /// <param name="pickupsToSpawn">Number of pickups to spawn.</param>
+        /// <param name="entityList">Transform which is to parent the spawned Pickups.</param>
+        void spawnPickups(int pickupsToSpawn, Transform entityList) {
+
+            // Generates positions for spawning enemies.
+            GenerateEntityPositions(pickupsToSpawn);
+
+            for (int i = 0; i < entityTiles.Count; i++) {
+
+                Vector3 pickupPos = entityTiles[i];
+                GameObject spawnedPickup = pickups[Random.Range(0, pickups.Length)];
+
+                pickupPos.y += (chunkCount * CHUNK_ROWS);
+
+                GameObject pickupInstance = Instantiate(spawnedPickup, pickupPos, Quaternion.identity) as GameObject;
+
+                pickupInstance.transform.SetParent(entityList);
+            }
+        }
+    }
