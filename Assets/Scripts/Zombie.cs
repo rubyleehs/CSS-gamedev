b732b44db21cc974482442b110f2f496f5687d73
rubyleehs <rubyleehs.dev@gmail.com>
2021-04-09 21:04:31 +0800

    workshop #2

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -10,7 +10,11 @@ using UnityEngine;
         /// <param name="target"> The Agent to attack. </param>
         public override bool Attack(Agent target)
         {
-        // TODO: trigger an animation called... and also the sound of... what should be dealt to the player...?
+            // TODO: trigger an animation called... and also the sound of... what should be dealt to the player...?
+            animator.SetTrigger("Attacking");
+            target.ChangeHpAmount(-attackDamage);
+            attackSFX.Play();
+
             return true;
         }
 
@@ -21,7 +25,8 @@ using UnityEngine;
         /// <returns> If this is able to attack the target under current conditions. </returns>
         public override bool CanAttack(Agent target)
         {
-            // TODO: change the true so that the enemy can attack the player only if it's adjacent else it will just keep on getting the green light to attack
-            return true;
+        // TODO: change the true so that the enemy can attack the player only if it's adjacent else it will just keep on getting the green light to attack
+            return Mathf.Abs((int)target.transform.position.x - (int)transform.position.x) 
+                + Mathf.Abs((int)target.transform.position.y - (int)transform.position.y) <= 1;
         }
     }
